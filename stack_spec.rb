f85f5257934a0cfa38efe89d958689806414e6cc
Neno Almeida <nenoxvx@icloud.com>
2016-06-30 12:02:27 -0300

    Create stack_spec.rb

---
+++ stack_spec.rb
@@ -0,0 +1,15 @@
+
+#first test Run this on your irb terminal
+
+describe Stack do
+  describe '#push' do
+    it 'puts an element at the top of the stack' do
+      stack = Stack.new
+
+      stack.push(1)
+      stack.push(2)
+
+      expect(stack.top).to eq(2)
+    end
+  end
+end
